063e9b37a682581a90c8b16e83afaf8095977639
Krutpik <sarafor670@gmail.com>
2024-07-19 16:11:49 +0300

    Редактирование темы

---
+++ Models/EditThemeModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeReddit.Models;
+
+public class EditThemeModel
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Title { get; set; }
+    [Required]
+    public string? Content { get; set; }
+
+}
